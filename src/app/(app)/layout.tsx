af46ec031f08c31ef76e3b111000888b06921e2f
Krish Gupta <shivkumar200510@gmail.com>
2025-09-28 06:28:46 +0000

    the content on all the ages of the dashboard is still at the bottom of t

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -14,9 +14,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           <AppSidebar />
         </Sidebar>
         <SidebarInset>
-          <div className="flex flex-col">
+          <div className="flex flex-col h-screen overflow-hidden">
             <AppHeader />
-            <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
+            <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8 bg-background">
               {children}
             </main>
           </div>
